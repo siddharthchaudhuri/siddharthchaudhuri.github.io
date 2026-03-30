6876993c243edcc9b5e2abac6484d807831f5b83
siddharthchaudhuri <chaudhurisiddharth24@gmail.com>
2026-03-31 03:13:35 +0530

    Create data.js

---
+++ data.js
@@ -0,0 +1,208 @@
+const portfolioData = {
+
+creativity: [
+
+{
+title: "Spec Ads",
+desc: "Would've Could've Should've",
+image: "https://images.unsplash.com/photo-1580907826414-5345a5aa68ad",
+items: [
+{ title: "KFC | English", url: "https://docs.google.com/presentation/d/1TkJzWFiDSZduiwr3WXeuj_yAXJZtJxYgHU42grRExqI/edit?usp=sharing" },
+{ title: "Olly | English", url: "https://docs.google.com/presentation/d/16x3HOnKIveE58Q9ZbIfTcsfx3tBBBOJkAvLMkrJ5siI/edit?usp=sharing" },
+{ title: "Casper | English", url: "https://docs.google.com/presentation/d/1crgikIbC654uu3lt7T_iRd4a0NzFRAmNOu5PUzejw2M/edit?usp=sharing" }
+]
+},
+
+{
+title: "Brand Pitches",
+desc: "Campaign-ready creativity",
+image: "https://images.unsplash.com/photo-1549281899-f75600a24107",
+items: [
+{ title: "Bobakat Pitch | English", url: "https://docs.google.com/presentation/d/1wmT9WiZ8lD6SsX0PZKAoiy7hvcKYXyiU/edit" },
+{ title: "Apsara Ice Cream Pitch | English + Hindi", url: "https://docs.google.com/presentation/d/1TDQfuX41TiGsqTE-CgpwKZTZKHwMeFz-d5djOjakjF8/edit?usp=sharing" },
+{ title: "Tribe Pitch | English + Hindi", url: "https://docs.google.com/presentation/d/1VzPZtLMSkiCoS4hv1BNO2NztnfvVyu5NAQ9DLlYxcWI/edit?usp=sharing" },
+{ title: "KDM Pitch | English + Hindi", url: "https://docs.google.com/presentation/d/1aeF9DVbnmBoZU3MCvSZzWHF7rFYTkslkG4lXYZuwqR4/edit?usp=sharing" },
+{ title: "Camlin Pitch | English + Hindi", url: "https://docs.google.com/presentation/d/16L28fI9Dle9RJbPJGeGIhqjJKRQQQoNH/edit" },
+{ title: "ZIGLY Pitch | English + Hindi", url: "https://docs.google.com/presentation/d/19bEpOJFjexwewrZlKrLVYvhg-_7EjgMt/edit" }
+]
+},
+
+{
+title: "Social Media Campaigns & Creatives",
+desc: "Scroll-stopping ideas and copy",
+image: "https://images.unsplash.com/photo-1553532435-93d532a45f15",
+subsections: [
+
+{
+title: "Social Media Campaigns",
+items: [
+{ title: "CG - Independence Day Mini Campaign | English", url: "https://docs.google.com/presentation/d/1hpN0wRj3HFa3JQN8mgGYw890vykHbWmhOCOnyRbtKcI/edit?usp=sharing" },
+{ title: "Bobakat - Valentine's Week Campaign | English", url: "https://docs.google.com/presentation/d/1M3NuDMqUNMQvFfp14LT65TLzXwowFORQnr7MbxGww7U/edit?usp=sharing" },
+{ title: "Bobakat - 50% Off Campaign | English", url: "https://docs.google.com/presentation/d/1tPdguOdNQl1XfIUPQdSd3-K8YWaf4IqUP5QZkf8Vuto/edit?usp=sharing" },
+{ title: "2baconil - Diwali Social Media Campaign | English + Hindi", url: "https://docs.google.com/presentation/d/1n0cRyMt-R0Su5ujouYB0XtFucv9jPGqYNxCQcsh4Jzg/edit?usp=sharing" }
+]
+},
+
+{
+title: "Standalone Posts",
+items: [
+{ title: "2baconil - Halloween | English", url: "https://drive.google.com/file/d/1oveqVhqAlxsRw9e5Az86YapX9X6VLfgu/view?usp=sharing" },
+{ title: "CG - Halloween | English", url: "https://drive.google.com/file/d/1POb2VEBpvO7jLRSN3n4MZ4JdFKc0Mx3u/view?usp=sharing" },
+{ title: "2baconil - Lung Test World Cancer Day | English", url: "https://drive.google.com/file/d/19hMdxD29QlkbyFXpPMLKL2LCohjpWWWN/view" },
+{ title: "Being Human Clothing - Accessories | English", url: "https://drive.google.com/file/d/14u1P9xcd33EGVqovyOWUTI38gx-dMssu/view" },
+{ title: "CG Power - Product Categories | English", url: "https://drive.google.com/file/d/1KvsGgIZ-RginILmaQR-OdSxbVayrCBFm/view" },
+{ title: "Never Grow Up x Good Wave - Fundraiser | English", url: "https://drive.google.com/file/d/1Lpn6UyTb-m3mJBlzoUdc2I1rAe_qRNtK/view" },
+{ title: "Stratzy - An Ideal Trader | English", url: "https://drive.google.com/file/d/1LgS0ko_R-rc0lCGoot-D9XPDGDSH-dHp/view" },
+{ title: "Winn Foods - Winner Winner Chicken Dinner | English", url: "https://drive.google.com/file/d/1-3S1Jyg66iXY98qi5ddvGnwTtzWPm4LE/view" },
+{ title: "Amazon - Bazaar Engagement | English + Hindi", url: "https://drive.google.com/file/d/1OG6CC1BJ0LIuDrKl24ggiQ_s5bqjgiGp/view" }
+]
+}
+
+]
+},
+
+{
+title: "Out Of House (OOH) Promotion",
+desc: "Making streets speak",
+image: "https://images.unsplash.com/photo-1513757378314-e46255f6ed16",
+items: [
+{ title: "Apsara Ice Creams", url: "https://docs.google.com/presentation/d/1BbgWdJdM9dMpXreFV4-qy_qLkoRPp5KqEABpukBcZPw/edit?usp=sharing" },
+{ title: "Bobakat x Lollapalooza - Store Branding", url: "https://docs.google.com/presentation/d/1fsMn1BfDNRgAQispzzGqQYDLolH0MuN9kKB3rzCXnv4/edit?usp=sharing" },
+{ title: "Beeing Social - Standee", url: "https://drive.google.com/file/d/1cpU9slEcCNOvQayP2G4gZshnOHpYNXfC/view" },
+{ title: "Dotom - 20 Minutes Accessibility Promotion", url: "https://drive.google.com/file/d/12CsGTW-ju5b-f7hYn8c5AQY_QUjkkMPs/view" }
+]
+},
+
+{
+title: "Professional Videos",
+desc: "Big ideas, beautifully told",
+image: "https://images.unsplash.com/photo-1577190651915-bf62d54d5b36",
+items: [
+{ title: "Stratzy - NIFTY Dip Podcast : Hindi", url: "https://drive.google.com/file/d/1mV_qdKqieWIJA_YgZMv5E7xMUddjkMWc/view" },
+{ title: "Stratzy - NIFTY Downfall Podcast : Hindi", url: "https://drive.google.com/file/d/1a1cP80MOp6ApkTs25zAeAVUYEwuBDTFG/view" },
+{ title: "Stratzy - Traders of India Podcast : Hindi", url: "https://drive.google.com/file/d/1Bn1McHbdnMIaG10kd0r7Hdib9dbsF2s_/view" },
+{ title: "CG - Extra Caring Fiance : Hindi", url: "https://drive.google.com/file/d/1AaqDbTtGqapR3UkAgr98JE2ssaIwW-7z/view?usp=sharing" },
+{ title: "Winn - Garba Madness : Hindi", url: "https://drive.google.com/file/d/1tFKU_YDE4pgR2enFZiuaG5fZ8z4Z5wnP/view?usp=sharing" }
+]
+}
+
+],
+
+content: [
+
+{
+title: "AI Music and Videos",
+desc: "Artificial Intelligence meets Authentic Emotion",
+image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRfQ64HCB6G8Ox2LeapjadP2AKLArIkEZE8Xw&s",
+items: [
+{ title: "Aposan - Brand Film", url: "https://drive.google.com/file/d/1te-UvFs0b5fLTq3B-boPrTC6sZdp1b9P/view?usp=sharing" },
+{ title: "2baconil - Christmas Song", url: "https://drive.google.com/file/d/11MZDR5agfSgcXDMn8N6-2lHca8hjZpZI/view" },
+{ title: "CG – Diwali Anthem", url: "https://drive.google.com/file/d/11MZDR5agfSgcXDMn8N6-2lHca8hjZpZI/view" },
+{ title: "CG – Ganpati AI Video", url: "https://drive.google.com/file/d/1kMd0XaMqv0FvKcJZjq1U-kGdkex8P7ZB/view" },
+{ title: "CG – New Year AI Video", url: "https://drive.google.com/file/d/1enf1Mf1Ptpa0_BD_ixeuVMfQs1zMhNQO/view" },
+{ title: "Camlin - Astronaut Dreams", url: "https://drive.google.com/file/d/1HILoxH1vQaKmkw7UyE72iSyFsb-TyGhG/view" },
+{ title: "Fraganta – Heer Ranjha Anthem", url: "https://drive.google.com/file/d/1aptBKxYHifuTPjTOY9pqX3gGuU92viJj/view" },
+{ title: "Fraganta – Jogi Anthem", url: "https://drive.google.com/file/d/19XOnzBwzm9877ODNgSrs4DiAbsQMxjN4/view" },
+{ title: "Winn – Halloween", url: "https://drive.google.com/file/d/1grUCan7Dj2Vl1YqRt4hPBh1kz1lXeydI/view" },
+{ title: "Winnn – Anthem", url: "https://drive.google.com/file/d/1juTog6a4Ewm9t5W1nbHOxdtlpvHnv1P_/view" }
+]
+},
+
+{
+title: "Website and App",
+desc: "Digital spaces with soul",
+image: "https://mockuptree.com/wp-content/uploads/edd/2018/12/Web-Showcase-Mockup-Creator.jpg",
+items: [
+{ title: "Bold & Bae - Website", url: "https://drive.google.com/file/d/1WYMGQglIuxFIaKci0Tkyk4fJWAhDCMc6/view" },
+{ title: "Fraganta - Website", url: "https://drive.google.com/file/d/1LYI4cIKCQBaWGOiIVIQgc61k0aoqzE4w/view" },
+{ title: "Stratzy - App Onboarding", url: "https://drive.google.com/file/d/10R4jyP3Efti-MJC9dzWk9dtkiohQ0cuC/view" },
+{ title: "Stratzy - Website", url: "https://drive.google.com/file/d/1OKaCpMGe5_3H6F2nb1NSDyXzVXJFKQJv/view" }
+]
+},
+
+{
+title: "Video Scripts",
+desc: "Stories made for screens",
+image: "https://images.unsplash.com/photo-1434030216411-0b793f4b4173",
+items: [
+{ title: "Amazon - Father’s Day", url: "https://docs.google.com/document/d/18rlQVBoYG0-23edJij0Hj3ABktu6lZWH/edit" },
+{ title: "HUL - Paani Ki Kahaani", url: "https://docs.google.com/document/d/1lAipVwlvYzEsMvGTywEiE39XmyomUeea/edit" },
+{ title: "Netflix - Recruitment Ad", url: "https://docs.google.com/document/d/1Uwff60KlBaDbrXwSV3Yx7nRqPXZs4q2A/edit" },
+{ title: "Never Grow Up - Mental Health Reel", url: "https://docs.google.com/document/d/1CpgbipjBq84miPsCvW_eFMQbr4LVwhWZ/edit" },
+{ title: "Never Grow Up - Women’s Day", url: "https://docs.google.com/document/d/1Ywjw9WP_mMLSLTLfbJc9LRpIO2ScTUBi/edit" },
+{ title: "Sanofi - Pride Month", url: "https://docs.google.com/document/d/1EjCzothVJm9T8SDdf8LfiNGj7BmoFRfb/edit" },
+{ title: "Stratzy - Husn Parody", url: "https://docs.google.com/document/d/1gqC6nhmktIB0h4r5tImBjw9Kcz5qHNHI/edit" }
+]
+}
+
+],
+
+extended: [
+
+{
+title: "Reels",
+desc: "Tiny films with big feelings",
+image: "https://images.unsplash.com/photo-1485846234645-a62644f84728",
+items: [
+{ title: "Bobakat - Christmas Week", url: "https://drive.google.com/file/d/12trTbC9ASrZSRy9eZB8I6VLzOGQtcexw/view?usp=sharing" },
+{ title: "Bold & Bae - Lonely", url: "https://drive.google.com/file/d/1fqUlwK77s-aBW5nihfzeZcrdAJM0dHsP/view" },
+{ title: "Bold & Bae - No Way", url: "https://drive.google.com/file/d/1JBkvQzVRe4gy_ziCpA4MMdaBLmqTnPdQ/view" },
+{ title: "Brandniti - Watchu Doin", url: "https://drive.google.com/file/d/1fcE0CMoIcts-OeMuDv_Ve5nLrcS6NJPu/view" },
+{ title: "Stratzy - Informational", url: "https://drive.google.com/file/d/1Li9RXWOpYNmN-QJlf39CqNjwtIQidIwt/view" },
+{ title: "Stratzy - Promotional", url: "https://drive.google.com/file/d/1LngSO56Qrsfelb2h9RBT3NVVP4W1Mcai/view" },
+{ title: "Stratzy - Windows SFX", url: "https://drive.google.com/file/d/1MAdSYeblDX7g8EIyTpOY6ZT-8QdfaIPl/view" },
+{ title: "Stratzy - Reporter Scam", url: "https://drive.google.com/file/d/1aMjCM9h5_GgvP8yP8qoFYZ6PMWJKe3GE/view" }
+]
+},
+
+{
+title: "Press Release and Media Profile",
+desc: "Brand stories, told to the world",
+image: "https://images.unsplash.com/photo-1504711434969-e33886168f5c",
+items: [
+{ title: "Fraganta Media Profile", url: "https://docs.google.com/document/d/1M8SziAGlFiP7q4sRxWnRk_6m36OOpJxk/edit" },
+{ title: "Supersox - 40 Under", url: "https://docs.google.com/document/d/1oYnz4SGVI99oR035jFSFeJLGGQxXIqXQ/edit" },
+{ title: "SuperSox - Budget Quote", url: "https://docs.google.com/document/d/1e94eBnKoFyXkO6L1FcBoz8X_W67-9eYz/edit" },
+{ title: "SuperSox - Company Profile", url: "https://docs.google.com/document/d/1u00qP1tFpjZd6iRrxOFzGsAVCSvQrO4b/edit" },
+{ title: "VIP - Chaddi Pe Gaddi PR", url: "https://docs.google.com/document/d/17Z2Ig_kjDm1mkaKJefB0Fnt2mhOzKj-l/edit" },
+{ title: "VIP - Kerala Blasters PR", url: "https://docs.google.com/document/d/1sD-irov1c-5TBOl-WgkaJC2uJQbT-zNa/edit" }
+]
+},
+
+{
+title: "Internal Communication",
+desc: "Where company messages find heart",
+image: "https://images.unsplash.com/photo-1521790361543-f645cf042ec4",
+items: [
+{ title: "CACTUS - Caffeine Conversations", url: "https://drive.google.com/file/d/1KKQdtXdPWisKd27XvbTeNcqdrPhmS-cD/view" },
+{ title: "HUL - Paani Ki Kahani", url: "https://drive.google.com/file/d/18KOttyIJNVkgxZYxRPEN272_qMrZph6y/view" },
+{ title: "Radix - Rewards Mailer", url: "https://drive.google.com/file/d/1Qek6RQArCsRPaai3vp3bH7cnX06j_96_/view" },
+{ title: "Writer Corp - LinkedIn Learning", url: "https://drive.google.com/file/d/1waE2dxOvyfmbvjkCVfJolysMh_x3XG8i/view" },
+{ title: "ZEE - Voting Phase 7", url: "https://drive.google.com/file/d/1M4dnVIg3pRMsdrWNSUDjBdnQ9eO--soc/view" },
+{ title: "ZEE5 - Blood Donation", url: "https://drive.google.com/file/d/1-IeQuy4uYwGOT4zZX8QlWrVF8nxqSyjx/view" }
+]
+},
+
+{
+title: "Chatbot Script",
+desc: "Conversations with character",
+image: "https://images.unsplash.com/photo-1684369175833-4b445ad6bfb5",
+items: [
+{ title: "Jadika Cargo Agencies - Chatbot Script", url: "https://drive.google.com/file/d/1KfWd0byplIF0R9E8IOQX74oWs2LaUyBn/view" }
+]
+},
+
+{
+title: "Merchandise",
+desc: "Reppable designs",
+image: "https://images.unsplash.com/photo-1642294490133-632a5b3bd36a",
+items: [
+{ title: "Never Grow Up - Don’t Assume Gender", url: "https://drive.google.com/file/d/1A5jottwIe6xYgq2OZqfRlI7ueiNRuueL/view" },
+{ title: "Never Grow Up - Non Binary", url: "https://drive.google.com/file/d/181cS7yB_SDqcfaFrPblQ04JqxhhywxGW/view" },
+{ title: "Never Grow Up - Genderfluid", url: "https://drive.google.com/file/d/1yTz-HoSs6eHwTvB4jkECSZUi9giNiHZZ/view" }
+]
+}
+
+]
+
+};
